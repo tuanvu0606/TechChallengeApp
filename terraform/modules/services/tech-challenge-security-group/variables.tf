45d90fa945f9a61cab841fb48cb78db6187fc062
Nhat Tuan Vu <tuanvu0695@gmail.com>
2022-01-26 02:07:05 +1100

    remove port 80

--- terraform/modules/services/tech-challenge-security-group/variables.tf
+++ terraform/modules/services/tech-challenge-security-group/variables.tf
@@ -1,3 +1,25 @@
 variable "vpc_id" {
   type = string
 }
+
+variable "ingress_rules" {
+  type    = list(string)
+  default = []
+}
+
+variable "egress_rules" {
+  type    = list(string)
+  default = [
+  {
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  },
+]
+}
